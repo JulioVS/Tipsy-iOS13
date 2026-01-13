517372db64f8bfac97af78b790e8401a89a7a511
JulioVS <julioer@me.com>
2026-01-12 21:37:26 -0300

    Add Outlets and Actions

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    var tipRate = 0.1
+    var splitNumber = 2.0
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    @IBAction func tipChanged(_ sender: UIButton) {
+        
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        
+        sender.isSelected = true
+        
+        switch sender.currentTitle {
+        case "0%":
+            tipRate = 0.0
+        case "10%":
+            tipRate = 0.1
+        case "20%":
+            tipRate = 0.2
+        default:
+            tipRate = 0.1
+        }
+        
     }
+    
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        
+        splitNumberLabel.text = String(format: "%.0f", sender.value)
+        splitNumber = sender.value
 
-
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        
+        print(tipRate)
+        print(String(format: "%.0f", splitNumber))
+        
+    }
+    
 }
 
